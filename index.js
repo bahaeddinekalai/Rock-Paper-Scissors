e2d94bddbbf04b87ffe303955af7a9e450e5c49b
bahaeddinekalai <bahaeddinekalai@gmail.com>
2023-03-29 03:58:40 +0100

    feat: add function to play one round
    This commit adds a new function called playRound to the rock-paper-scissors game.
    The function allows the player and computer to play one round
    of the game and displays the result to the console.
    This function is essential to the rock-paper-scissors game and
    provides a foundation for the game's logic.

--- index.js
+++ index.js
@@ -29,6 +29,53 @@ function getPlayerChoice() {
   } else {
     getPlayerChoice();
   }
-};
+}
+
+function playRound(playerChoice, computerSelection) {
+  if (computerSelection == "SCISSORS") {
+    switch (playerChoice) {
+      case "ROCK":
+        return "You Win! Rock beats Scissors";
+
+      case "PAPER":
+        return "You Lose! Scissors beats Paper ";
+
+      case "SCISSORS":
+        return "Tie";
+
+      default:
+        return "Tie";
+    }
+  } else if (computerSelection == "PAPER") {
+    switch (playerChoice) {
+      case "ROCK":
+        return "You Lose! Paper beats Rock";
+
+      case "PAPER":
+        return "Tie";
+
+      case "SCISSORS":
+        return "You Win! Scissors beats Paper ";
+
+      default:
+        return "Tie";
+    }
+  } else {
+    switch (playerChoice) {
+      case "ROCK":
+        return "Tie";
+
+      case "PAPER":
+        return "You Win! Paper beats Rock";
+
+      case "SCISSORS":
+        return "You Lose! Rock beats Scissors";
+
+      default:
+        return "Tie";
+    }
+  }
+}
 const computerSelection = getComputerChoice();
 const playerSelection = getPlayerChoice();
+console.log(playRound(playerSelection, computerSelection));
