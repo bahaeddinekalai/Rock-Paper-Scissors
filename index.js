083eacb8bd850e7bb07fd9699903ef3dc7136eec
bahaeddinekalai <bahaeddinekalai@gmail.com>
2023-03-29 03:10:37 +0100

    feat: add function to get player's choice
    This commit adds a new function named getPlayerChoice to the index.js file.
    The function prompts the user to choose between
    - Rock
    - Paper
    - Scissors
    This function is necessary for the game to allow the user to make a choice.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let getComputerChoice = () => {
+function getComputerChoice() {
   let randomChoice = Math.floor(Math.random() * 3) + 1;
   switch (randomChoice) {
     case 1:
@@ -16,5 +16,19 @@ let getComputerChoice = () => {
       break;
   }
   return getComputerChoice;
+}
+function getPlayerChoice() {
+  let playerChoice = prompt("choose between Rock ,Paper and Scissors", "");
+  playerChoice = playerChoice.toUpperCase();
+  if (
+    playerChoice == "ROCK" ||
+    playerChoice == "SCISSORS" ||
+    playerChoice == "PAPER"
+  ) {
+    return playerChoice;
+  } else {
+    getPlayerChoice();
+  }
 };
-alert(getComputerChoice());
+const computerSelection = getComputerChoice();
+const playerSelection = getPlayerChoice();
